34885677516f8e94c87a718ffe3ed5af40be2871
jpatrick5402 <jpatrick5402@gmail.com>
2024-06-12 14:55:15 -0400

    Completed email functionality from the contact me section

--- assets/js/submit.js
+++ assets/js/submit.js
@@ -2,6 +2,18 @@ var form = document.getElementById("formId");
 
 function submitForm(event) {
   event.preventDefault();
+  var email = document.getElementById("outEmail");
+  var content = email.attributes.getNamedItem("href");
+  content.value = content.value
+    .replace("SubjectReplace", document.getElementById("subject").value)
+    .replace("BodyReplace", document.getElementById("message").value);
+  content.value +=
+    "%0A%0A" +
+    document.getElementById("name").value +
+    "%0A" +
+    document.getElementById("email").value;
+  console.log(content);
+  email.click();
 }
 
 form.addEventListener("submit", submitForm);
