60990d983fc39012507253e6010ced5d5f3af1d0
jpatrick5402 <jpatrick5402@gmail.com>
2024-12-30 11:58:26 -0500

    Make heading-icon spin on click using js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,12 @@
 function load() {}
+
+let position = 0;
+function rotate(element) {
+    position += 90;
+    element.style.transform = `rotate(${position}deg)`;
+}
+
+let el = document.getElementById("heading-icon");
+el.addEventListener("click", function () {
+    rotate(el);
+});
